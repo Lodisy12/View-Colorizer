7b46bff1023b9c142019f15b246cbc6117d89df7
Joey Lawter <42898462+Lodisy12@users.noreply.github.com>
2019-10-01 14:28:51 -0700

    Changed box ids. Added listener on click function

--- app/src/main/java/com/example/viewcolorizer/MainActivity.kt
+++ app/src/main/java/com/example/viewcolorizer/MainActivity.kt
@@ -2,11 +2,27 @@ package com.example.viewcolorizer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setListeners()
+    }
+
+    private fun setListeners() {
+        val clickableViews: List<View> = listOf(box_one_text, box_two_text, box_three_text, 
+            box_four_text, box_five_text)
+        
+        for (item in clickableViews) {
+            item.setOnClickListener { makeColored(it) }
+        }
+    }
+
+    private fun makeColored(it: View?) {
+
     }
 }
